e1a498c0401801d7b72802724d0820817b4e3818
Henry Swanson <henryswanson94@gmail.com>
2021-03-01 02:43:35 -0800

    Add Stumpff functions
    I know I'll need these in the future; let's knock them out real quick.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate kiss3d;
 
+mod stumpff;
+
 use kiss3d::camera::ArcBall;
 use kiss3d::light::Light;
 use kiss3d::window::Window;
@@ -8,22 +10,25 @@ use kiss3d::nalgebra as na;
 use na::Point3;
 
 fn main() {
-    // Set up window and camera
-    let mut window = Window::new("Kiss3d: cube");
-    let mut camera = ArcBall::new(Point3::new(0.0, -10.0, 10.0), Point3::origin());
-    window.set_light(Light::StickToCamera);
+    // // Set up window and camera
+    // let mut window = Window::new("Kiss3d: cube");
+    // let mut camera = ArcBall::new(Point3::new(0.0, -10.0, 10.0), Point3::origin());
+    // window.set_light(Light::StickToCamera);
 
-    // Create Kerbin
-    let mut kerbin = window.add_sphere(1.0);
-    kerbin.set_color(0.0, 1.0, 0.0);
-    // Compute circle path to draw
-    let circle_pts = construct_circle(2.0, 100);
+    // // Create Kerbin
+    // let mut kerbin = window.add_sphere(1.0);
+    // kerbin.set_color(0.0, 1.0, 0.0);
+    // // Compute circle path to draw
+    // let circle_pts = construct_circle(2.0, 100);
 
-    // Render loop
-    while window.render_with_camera(&mut camera) {
-        draw_grid(&mut window, 20, 1.0, &Point3::new(0.5, 0.5, 0.5));
-        draw_loop(&mut window, &circle_pts, &Point3::new(1.0, 0.0, 0.0));
-    }
+    // // Render loop
+    // while window.render_with_camera(&mut camera) {
+    //     draw_grid(&mut window, 20, 1.0, &Point3::new(0.5, 0.5, 0.5));
+    //     draw_loop(&mut window, &circle_pts, &Point3::new(1.0, 0.0, 0.0));
+    // }
+    stumpff::chebyshev::generate_stumpff_coefficients(2, 0.6);
+    stumpff::chebyshev::generate_stumpff_coefficients(3, 0.2);
+    stumpff::plotting::draw_plots();
 }
 
 fn draw_grid(window: &mut Window, num_squares: i32, square_size: f32, color: &Point3<f32>) {
