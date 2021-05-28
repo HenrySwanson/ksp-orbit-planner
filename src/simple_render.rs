066de0434ac38ccb5d802132eb2863d9e9658fc7
Henry Swanson <henryswanson94@gmail.com>
2021-05-28 04:49:43 -0700

    Handle "colored lines" in a uniform way

--- src/simple_render.rs
+++ src/simple_render.rs
@@ -10,19 +10,29 @@ use std::f64::consts::PI;
 
 use crate::body::BodyState;
 use crate::camera::CustomCamera;
+use crate::orbit::Orbit;
 use crate::universe::{BodyID, Frame, ShipID, Universe};
+
+pub struct Path {
+    nodes: Vec<Point3<f32>>,
+    frame: Frame,
+    color: Point3<f32>,
+}
+
+pub enum FocusPoint {
+    Body(BodyID),
+    // TODO add ship focus once you add ship frames...
+}
 
 pub struct Scene {
     universe: Universe,
     body_spheres: HashMap<BodyID, SceneNode>,
-    orbit_paths: HashMap<BodyID, Vec<Vector3<f32>>>,
-
     ship_objects: HashMap<ShipID, SceneNode>,
-    ship_paths: HashMap<ShipID, Vec<Vector3<f32>>>,
+    paths: Vec<Path>,
 
     window: Window,
     camera: CustomCamera,
-    camera_focus_order: Vec<BodyID>,
+    camera_focus_order: Vec<FocusPoint>,
     camera_focus_idx: usize,
 
     timestep: f64,
@@ -34,54 +44,77 @@ impl Scene {
         // Set up camera
         let camera = CustomCamera::new(2.0e9);
         let camera_focus_idx: usize = 0;
-        let mut camera_focus_order: Vec<BodyID> = universe.body_ids().copied().collect();
-        camera_focus_order.sort();
+        // TODO sort focus points in a more systematic way
+        let mut camera_focus_order: Vec<FocusPoint> =
+            universe.body_ids().map(|x| FocusPoint::Body(*x)).collect();
+        camera_focus_order.sort_by_key(|f| match f {
+            FocusPoint::Body(x) => *x,
+        });
+
+        let mut paths = vec![];
 
         // Collect planetary bodies
         let mut body_spheres = HashMap::with_capacity(universe.bodies.len());
-        let mut orbit_paths = HashMap::with_capacity(universe.bodies.len());
         for (id, body) in universe.bodies.iter() {
             // Make the sphere that represents the body
             let mut sphere = window.add_sphere(body.info.radius);
             let color = &body.info.color;
             sphere.set_color(color.x, color.y, color.z);
-
-            // Compute the path to draw for the orbit
-            let path: Vec<Vector3<f32>> = match body.get_orbit() {
-                Some(orbit) => {
-                    (0..=100_usize)
-                        // start at -180 so that open orbits work right
-                        .map(|i| -PI + (2.0 * PI) * (i as f64) / 100.0)
-                        .map(|theta| orbit.get_position_at_theta(theta))
-                        .filter_map(|option_v| option_v.map(na::convert))
-                        .collect()
-                }
-                None => vec![],
-            };
-
             body_spheres.insert(*id, sphere);
-            orbit_paths.insert(*id, path);
+
+            // Compute the path to draw for the orbit, if relevant
+            let (parent_id, state) = match &body.state {
+                BodyState::FixedAtOrigin => continue,
+                BodyState::Orbiting { parent_id, state } => (parent_id, state),
+            };
+            let orbit = state.get_orbit();
+            paths.push(Path {
+                nodes: get_path_for_orbit(&orbit, 100),
+                frame: Frame::BodyInertial(*parent_id),
+                color: body.info.color,
+            });
+
+            // TODO remove, or somehow make optional
+            // Make axes that show the planet's orbits orientation
+            let make_axis_path = |v, color| -> Path {
+                let v: Vector3<f32> = na::convert(v);
+                let v = 2.0 * body.info.radius * v;
+                let pt: Point3<f32> = Point3::from(v);
+                Path {
+                    nodes: vec![Point3::origin(), pt],
+                    frame: Frame::BodyInertial(*id),
+                    color,
+                }
+            };
+            paths.push(make_axis_path(
+                orbit.periapse_vector(),
+                Point3::new(1.0, 0.0, 0.0),
+            ));
+            paths.push(make_axis_path(
+                orbit.asc_node_vector(),
+                Point3::new(0.0, 1.0, 0.0),
+            ));
+            paths.push(make_axis_path(
+                orbit.normal_vector(),
+                Point3::new(0.0, 0.0, 1.0),
+            ));
         }
 
         // Collect ships
         let mut ship_objects = HashMap::with_capacity(universe.ships.len());
-        let mut ship_paths = HashMap::with_capacity(universe.ships.len());
         for (id, ship) in universe.ships.iter() {
             // Make the cube that represents the ship
             let mut cube = window.add_cube(1e6, 1e6, 1e6);
             cube.set_color(1.0, 1.0, 1.0);
+            ship_objects.insert(*id, cube);
 
             // Compute the path to draw for the orbit
-            let orbit = ship.state.get_orbit();
-            let path: Vec<Vector3<f32>> = (0..=100_usize)
-                // start at -180 so that open orbits work right
-                .map(|i| -PI + (2.0 * PI) * (i as f64) / 100.0)
-                .map(|theta| orbit.get_position_at_theta(theta))
-                .filter_map(|option_v| option_v.map(na::convert))
-                .collect();
-
-            ship_objects.insert(*id, cube);
-            ship_paths.insert(*id, path);
+            let nodes = get_path_for_orbit(&ship.state.get_orbit(), 100);
+            paths.push(Path {
+                nodes,
+                frame: Frame::BodyInertial(ship.parent_id),
+                color: Point3::new(1.0, 1.0, 1.0),
+            });
         }
 
         // We can't query the fps, so let's just set it
@@ -90,9 +123,8 @@ impl Scene {
         Scene {
             universe,
             body_spheres,
-            orbit_paths,
             ship_objects,
-            ship_paths,
+            paths,
             window,
             camera,
             camera_focus_order,
@@ -169,37 +201,19 @@ impl Scene {
         // Draw grid
         draw_grid(&mut self.window, 20, 1.0e9, &Point3::new(0.5, 0.5, 0.5));
 
-        // Draw orbits
-        for (id, body) in self.universe.bodies.iter() {
-            let parent_id = match &body.state {
-                BodyState::FixedAtOrigin => continue,
-                BodyState::Orbiting { parent_id, .. } => *parent_id,
-            };
-            // Get the parent's position within the camera frame
-            let parent_position = convert_f32(
-                self.universe
-                    .get_body_position(parent_id, self.camera_frame()),
-            );
-            let orbit_path: Vec<_> = self.orbit_paths[id]
+        // Draw paths
+        for path in self.paths.iter() {
+            // Get the reference position for the path you're about to draw
+            let transform = self
+                .universe
+                .convert_frames(path.frame, self.camera_frame());
+            let transform: na::Isometry3<f32> = na::convert(transform);
+            let transformed_path: Vec<_> = path
+                .nodes
                 .iter()
-                .map(|x| parent_position + x)
+                .map(|p| transform.transform_point(p))
                 .collect();
-            draw_path(&mut self.window, &orbit_path, &body.info.color);
-        }
-
-        // Draw orbits for ships
-        for (id, ship) in self.universe.ships.iter() {
-            let parent_id = ship.parent_id;
-            // Get the parent's position within the camera frame
-            let parent_position = convert_f32(
-                self.universe
-                    .get_body_position(parent_id, self.camera_frame()),
-            );
-            let orbit_path: Vec<_> = self.ship_paths[id]
-                .iter()
-                .map(|x| parent_position + x)
-                .collect();
-            draw_path(&mut self.window, &orbit_path, &Point3::new(1.0, 1.0, 1.0));
+            draw_path(&mut self.window, &transformed_path, &path.color);
         }
 
         // Draw text
@@ -251,7 +265,9 @@ Orbit:
     }
 
     fn camera_frame(&self) -> Frame {
-        Frame::BodyInertial(self.camera_focus_order[self.camera_focus_idx])
+        match self.camera_focus_order[self.camera_focus_idx] {
+            FocusPoint::Body(id) => Frame::BodyInertial(id),
+        }
     }
 }
 
@@ -291,4 +307,17 @@ fn draw_path(window: &mut Window, pts: &[Point3<f32>], color: &Point3<f32>) {
     for pair in pts.windows(2) {
         window.draw_line(&pair[0], &pair[1], &color);
     }
+}
+
+fn get_path_for_orbit(orbit: &Orbit, num_points: usize) -> Vec<Point3<f32>> {
+    let mut pts = vec![];
+    for i in 0..=num_points {
+        let theta = -PI + (2.0 * PI) * (i as f64) / (num_points as f64);
+        let pt = match orbit.get_position_at_theta(theta) {
+            Some(v) => Point3::from(v),
+            None => continue,
+        };
+        pts.push(na::convert(pt));
+    }
+    pts
 }
